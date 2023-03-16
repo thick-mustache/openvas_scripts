6ea2f5cb8433f67c70fe6fb4b3d5ca9bcd8f233a
R. Silva | thick-mustache <116608062+thick-mustache@users.noreply.github.com>
2023-03-16 16:00:22 -0300

    Create full.py

---
+++ full.py
@@ -0,0 +1,23 @@
+import sys
+
+from gvm.connections import UnixSocketConnection
+from gvm.errors import GvmError
+from gvm.protocols.gmpv224 import Gmp
+from gvm.transforms import EtreeCheckCommandTransform
+
+path = '/tmp/gvm/gvmd/gvmd.sock'
+connection = UnixSocketConnection(path=path)
+transform = EtreeCheckCommandTransform()
+
+nome = 'teste1'
+ports = '33d0cd82-57c6-11e1-8ed1-406186ea4fc5'
+
+try:
+
+    with Gmp(connection=connection, transform=transform) as gmp:
+        gmp.authenticate('admin', 'admin')
+        
+        gmp.create_target(name=nome, hosts='192.168.0.1, 192.168.0.2', port_list_id=ports, alive_test='Consider Alive')
+
+except GvmError as e:
+    print('An error occurred', e, file=sys.stderr)
