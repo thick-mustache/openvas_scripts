04315cabcc8108d6644fb6d19916b3cfc34c0b72
R. Silva | thick-mustache <116608062+thick-mustache@users.noreply.github.com>
2023-03-16 10:28:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,22 @@
-from gvm.protocols import gmp
+import sys
 
-status = gmp.getversion().get('status')
+from gvm.connections import UnixSocketConnection
+from gvm.errors import GvmError
+from gvm.protocols.gmpv224 import Gmp
+from gvm.transforms import EtreeCheckCommandTransform
 
-print(status)
+path = '/tmp/gvm/gvmd/gvmd.sock'
+connection = UnixSocketConnection(path=path)
+transform = EtreeCheckCommandTransform()
+
+
+try:
+
+    with Gmp(connection=connection, transform=transform) as gmp:
+
+        version = gmp.get_version().get('session')
+
+        print(version)
+
+except GvmError as e:
+    print('An error occurred', e, file=sys.stderr)
